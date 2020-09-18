bdd1dadbcc201f6f48a0e1f525d2128590a9e421
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-18 21:45:24 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/learning/springjwtbasic/SpringJwtBasicApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.springjwtbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vijaygenius123/learning/springjwtbasic/SpringJwtBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.springjwtbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
